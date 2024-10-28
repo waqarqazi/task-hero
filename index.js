e30085c13e63993a835e52083de1eab4663b4bf6
waqarqazi <waqarhussain4154@gmail.com>
2024-10-28 14:44:06 +0400

    fixes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const routes = require('./routes');
-const port = process.env.PORT;
+const port = 3000;
 const app = express();
 
 app.use(
